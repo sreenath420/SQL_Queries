3b7a842fa7c2dae5fbfe2443dfa60f1126ee9aec
SreeNath <gootysreenathreddy@gmail.com>
2024-02-03 22:08:25 +0530

    Add files via upload

---
+++ query_sub.sql
@@ -0,0 +1,31 @@
+
+
+input
+
+name        opt_in
+
+john         true
+john         false
+john         false
+john         false
+kek          true
+kek          true                           
+kek          true
+
+
+
+output
+
+name     opt_in
+
+john     false
+kek      true
+
+
+select name,opt_in from (
+select name,
+opt_in,
+row_number()over(partition by name order by opt_in) as rnk
+from  john_kek
+) as tem where rnk=2
+
