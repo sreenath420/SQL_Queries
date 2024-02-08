a61cab7a581fbfd6b4bb9a6140157637f2e64662
SreeNath <gootysreenathreddy@gmail.com>
2024-02-09 00:19:17 +0530

    Add files via upload

---
+++ query_leetcode_artical_views.sql
@@ -0,0 +1,37 @@
+Table: Views
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+ 
+ 
+ | article_id | author_id | viewer_id | view_date  |
+| ---------- | --------- | --------- | ---------- |
+| 1          | 3         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |
+| 3          | 4         | 4         | 2019-07-21 |
+
+
+
+#Write your MySQL query statement below
+select distinct viewer_id as id from views
+where viewer_id=author_id 
+order by viewer_id
+
+output
+| id |
+| -- |
+| 4  |
+| 7  |
