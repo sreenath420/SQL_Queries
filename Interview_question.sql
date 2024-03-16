1bde195b8a228168d3749f155b8aba445330d8e8
SreeNath <gootysreenathreddy@gmail.com>
2024-03-17 00:54:56 +0530

    Update Interview_question.sql

--- Interview_question.sql
+++ Interview_question.sql
@@ -4,12 +4,12 @@
 Query
 
 select id from employee group by id having count(id)>1;
-
+--------------------------------------------------------------------------------------------------------------------------------------------
 2.select increment salary sal=10/100=0.1*sal+sal
 Query
 
 select id,name,sal,((0.1*sal)+sal) as incre_sal from employee;
-
+---------------------------------------------------------------------------------------------------------------------------------------------
 3.Second highest salary
 
 with cte as
@@ -17,7 +17,7 @@ with cte as
 select emp_name,salary,dense_rank() over(order by salay desc) as rnk from emp
 )
 select salary from cte where rnk=3
-
+-----------------------------------------------------------------------------------------------------------------------------------------------
 4.Delete duplicate records from Employee table(e-id) column
 
 Delete from employee
@@ -25,16 +25,17 @@ Where id not in (
 Select max(id) from employee 
 Group by e_name,e_address,e_age
 )
-
+------------------------------------------------------------------------------------------------------------------------------------------------------
 5.wirte query to fetch only present one time in given values
 select name,count(name) from city11 group by name having count(name)=1
-
-#write a query to find latest city visited customer
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+write a query to find latest city visited customer
 6.select id,name,location,date
 from customer_location loc
 inner join (
 select id as lid,max(date) from customer_location 
 group by id) lat on loc.id=lat.lid and location='hyd'
+--------------------------------------------------------------------------------------------------------------------------------------------------------
 7. write a query sql convert first letter caps and lower letter
 ename
 aLEn
