9c092fa29bf24d337e5f51dbebe98e286a4ef8ce
SreeNath <gootysreenathreddy@gmail.com>
2024-04-04 23:56:35 +0530

    Update query_case.sql

--- query_case.sql
+++ query_case.sql
@@ -48,10 +48,32 @@ from customer
 group by store
 order by store;
 
+Example:-2
+you're given the table on user viewership categorised by device type where the three types are laptop, tablet, and phone.
 
+Write a query that calculates the total viewership for laptops and mobile devices where mobile is defined as the sum of tablet and phone viewership. Output the total viewership for laptops as laptop_reviews and the total viewership for mobile devices as mobile_views.
 output
 ------------------------------------------------------------------------------------
 store  first_week    second_week   third_week
 101	    218	          null	        123
 102	    366	          282		        309
 105	    133	          59		        60
+
+user_id	  device_type		view_time
+---------------------------------------------------------------
+123  	 	tablet			01/02/2022 00:00:00
+125			laptop			01/07/2022 00:00:00
+128			laptop			02/09/2022 00:00:00
+129			phone			  02/09/2022 00:00:00
+145			tablet			02/24/2022 00:00:00
+
+
+Example Output
+---------------------------------------
+laptop_views	mobile_views
+2					        3
+
+SELECT sum(case WHEN device_type='laptop' THEN 1 else 0 end) as laptop_views,
+sum(case WHEN device_type in('tablet','phone') THEN 1 else 0 end) as mobile_views
+FROM viewership
+
