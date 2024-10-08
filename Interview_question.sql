382273b53b2ddfebc81415ad42c6ebcec8c17e0c
SreeNath <gootysreenathreddy@gmail.com>
2024-10-08 23:13:33 +0530

    Update Interview_question.sql

--- Interview_question.sql
+++ Interview_question.sql
@@ -518,5 +518,66 @@ end as node_type
 from manager;
 
 
-	
+
+------------------------------------->matching salary and deparment_name write query in sql<-------------------------------------------------
+
+create table employee
+( emp_ID int primary key
+, emp_NAME varchar(50) not null
+, DEPT_NAME varchar(50)
+, SALARY int);
+ 
+insert into employee values(101, 'Mohan', 'Admin', 4000);
+insert into employee values(102, 'Rajkumar', 'HR', 3000);
+insert into employee values(103, 'Akbar', 'IT', 4000);
+insert into employee values(104, 'Dorvin', 'Finance', 6500);
+insert into employee values(105, 'Rohit', 'HR', 3000);
+insert into employee values(106, 'Rajesh',  'Finance', 5000);
+insert into employee values(107, 'Preet', 'HR', 7000);
+insert into employee values(108, 'Maryam', 'Admin', 4000);
+insert into employee values(109, 'Sanjay', 'IT', 6500);
+insert into employee values(110, 'Vasudha', 'IT', 7000);
+insert into employee values(111, 'Melinda', 'IT', 8000);
+insert into employee values(112, 'Komal', 'IT', 10000);
+insert into employee values(113, 'Gautham', 'Admin', 2000);
+insert into employee values(114, 'Manisha', 'HR', 3000);
+insert into employee values(115, 'Chandni', 'IT', 4500);
+insert into employee values(116, 'Satya', 'Finance', 6500);
+insert into employee values(117, 'Adarsh', 'HR', 3500);
+insert into employee values(118, 'Tejaswi', 'Finance', 5500);
+insert into employee values(119, 'Cory', 'HR', 8000);
+insert into employee values(120, 'Monica', 'Admin', 5000);
+insert into employee values(121, 'Rosalin', 'IT', 6000);
+insert into employee values(122, 'Ibrahim', 'IT', 8000);
+insert into employee values(123, 'Vikram', 'IT', 8000);
+insert into employee values(124, 'Dheeraj', 'IT', 11000);
+
+
+
+
+
+
+SELECT  e1.emp_ID,e2.emp_ID, e1.emp_NAME, e1.DEPT_NAME, e1.SALARY
+FROM employees e1
+JOIN employees e2 
+ON e1.DEPT_NAME = e2.DEPT_NAME 
+AND e1.SALARY = e2.SALARY
+AND e1.emp_ID!=e2.emp_ID
+order by e1.DEPT_NAME;
+
+output:-
+
+
+108	Maryam	Admin	4000
+101	Mohan	Admin	4000
+116	Satya	Finance	6500
+104	Dorvin	Finance	6500
+114	Manisha	 HR	3000
+105	Rohit	 HR	3000
+102	Rajkumar HR	3000
+105	Rohit	 HR	3000
+122	Ibrahim	 IT	8000
+123	Vikram	 IT	8000
+111	Melinda	 IT	8000
+
 
